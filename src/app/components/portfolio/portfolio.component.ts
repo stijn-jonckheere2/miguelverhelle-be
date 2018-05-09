d59b8607e296a2b1e608b3533e126b97d04e2e0a
Stijn Jonckheere <>
2018-05-09 11:31:44 +0200

    portfolio page added

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { portfolioImages } from './portfolioImages';
 
 @Component({
   selector: 'app-portfolio',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
+  portfolioImages = portfolioImages;
+  portfolioLength = this.portfolioImages.length;
+  activeImage;
+
+  columnOneImages = [];
+  columnTwoImages = [];
+  columnThreeImages = [];
+  columnFourImages = [];
 
   constructor() { }
 
   ngOnInit() {
+    for (let i = 0; i < this.portfolioLength; i += 4) {
+      this.columnOneImages.push(this.portfolioImages[i]);
+    }
+    for (let i = 1; i < this.portfolioLength; i += 4) {
+      this.columnTwoImages.push(this.portfolioImages[i]);
+    }
+    for (let i = 2; i < this.portfolioLength; i += 4) {
+      this.columnThreeImages.push(this.portfolioImages[i]);
+    }
+    for (let i = 3; i < this.portfolioLength; i += 4) {
+      this.columnFourImages.push(this.portfolioImages[i]);
+    }
+  }
+
+  setActiveImage(image) {
+    this.activeImage = image;
+  }
+
+  resetActiveImage() {
+    this.activeImage = null;
   }
 
 }
---
+++ src/app/components/portfolio/portfolioImages.ts
@@ -0,0 +1,56 @@
+export const portfolioImages: {
+    src: string,
+    title: string,
+    description: string
+}[] = [
+        {
+            src: '/assets/images/exterieur/ext-1.png',
+            title: 'Exterieur',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit dapibus nulla, eu gravida est.'
+        },
+        {
+            src: '/assets/images/exterieur/ext-2.png',
+            title: 'Exterieur',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit dapibus nulla, eu gravida est.'
+        },
+        {
+            src: '/assets/images/exterieur/ext-3.png',
+            title: 'Exterieur',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit dapibus nulla, eu gravida est.'
+        },
+        {
+            src: '/assets/images/exterieur/ext-4.jpg',
+            title: 'Exterieur',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit dapibus nulla, eu gravida est.'
+        },
+        {
+            src: '/assets/images/interieur/int-1.png',
+            title: 'Interieur',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit dapibus nulla, eu gravida est.'
+        },
+        {
+            src: '/assets/images/interieur/int-2.png',
+            title: 'Interieur',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit dapibus nulla, eu gravida est.'
+        },
+        {
+            src: '/assets/images/interieur/int-3.png',
+            title: 'Interieur',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit dapibus nulla, eu gravida est.'
+        },
+        {
+            src: '/assets/images/interieur/int-4.png',
+            title: 'Interieur',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit dapibus nulla, eu gravida est.'
+        },
+        {
+            src: '/assets/images/interieur/int-5.png',
+            title: 'Interieur',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit dapibus nulla, eu gravida est.'
+        },
+        {
+            src: '/assets/images/interieur/int-6.png',
+            title: 'Interieur',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit dapibus nulla, eu gravida est.'
+        },
+    ];
