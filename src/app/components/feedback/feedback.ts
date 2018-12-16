6a52e09c41cf4e1b07e772b06b2e5a9d42d4ed2a
Stijn Jonckheere <jonckheere_stijn@hotmail.com>
2018-12-16 12:32:30 +0100

    added new feedback to the website

--- src/app/components/feedback/feedback.ts
+++ src/app/components/feedback/feedback.ts
@@ -1,4 +1,36 @@
+// tslint:disable:max-line-length
+// tslint:disable:no-trailing-whitespace
+
 export const feedback = [
+    {
+        title: 'Pierre Fivet - Directeur Vacansoleil België & Frankrijk',
+        date: new Date(2018, 11, 9),
+        content: `
+        Via een collega - ook een tevreden klant van Miguel - kwamen we bij Miguel Verhelle terecht. 
+        Wij hadden het plan een volledig nieuwe verdiep in te richten als nieuw hoofdkantoor voor Vacansoleil. 
+        
+        Heel snel kregen kregen we een correcte offerte met een correct prijsvoorstel. 
+        Samen hadden we een timing opgesteld , deze timing is tiptop aangehouden . <br/>
+        We hebben Miguel ervaren als een echte vakman, die ook telkens goed met klant meedenkt. 
+        En het schilderwerk was gewoon tiptop !  Kortom : een vakman met vooral veel liefde voor zijn vak. <br/>
+        Puik werk Miguel!`
+    },
+    {
+        title: 'Familie Rabaeys uit Adegem',
+        date: new Date(2018, 11, 9),
+        content: `
+        Dit komt recht uit ons hart! <br/><br/>
+
+        Bij het herinrichten van onze woning riepen we de hulp in van Miguel Verhelle.
+        Nadat hij het huis van mijn ouders aanpakte, leek hij ons dé schilder om opnieuw onder de arm te nemen.
+        In de hal bracht hij een gedurfd behang aan dat supergoed past bij de nieuwe kleur van de trap.
+        In ons werkbureau koos hij voor het behouden van de beuken kasten. De venstertabletten en legboorden schilderde hij wit. Hij maakte geen onnodige kosten.
+        <br/>6 weken een stielman in huis. We wisten amper dat Miguel hier was. Elke avond zorgde hij ervoor dat de woonruimtes leefbaar waren.
+        Hij bracht ons op de hoogte van datgene wat de volgende dag op zijn planning stond.
+        Wij lieten hem al een volgende prijsofferte maken want hebben alle vertrouwen in Miguel!
+
+        `
+    },
     {
         title: 'Familie Verbeest uit Roeselare',
         date: new Date(2016, 11, 2),
@@ -10,7 +42,7 @@ export const feedback = [
         title: 'Familie Delanghe uit Bredene',
         date: new Date(2016, 8, 15),
         content: `
-        Ik wou dit nog eens kwijt! Deze zomer onze ramen en keuken laten schilderen door Miguel Verhelle, een schat van een stielman die weet waarover hij praat en vol liefde zijn schilderskwast hanteert! Hij staat ook open voor eigen inbreng en geeft vrijblijvend professioneel advies. Het was voor ons alvast een vlotte samenwerking met prima resultaat! We zullen hem zeker nog contacteren voor andere projecten. Een dikke merci aan Miguel Verhelle.
+        Ik wou dit nog eens kwijt! Deze zomer onze ramen en keuken laten schilderen door Miguel Verhelle, een schat van een stielman die weet waarover hij praat en vol liefde zijn schilderskwast hanteert! Hij staat ook open voor eigen inbreng en geeft vrijblijvend professioneel advies. <br/>Het was voor ons alvast een vlotte samenwerking met prima resultaat! We zullen hem zeker nog contacteren voor andere projecten. Een dikke merci aan Miguel Verhelle.
         `
     },
     {
